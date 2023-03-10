8a9ca1ccc5551174bedf730f9e3c6dfd24fcedaf
Nicob04 <107031363+Nicob04@users.noreply.github.com>
2023-03-10 13:27:16 +0000

    ssss

--- funx/furry.py
+++ funx/furry.py
@@ -9,22 +9,22 @@ def furry(hp, bar):
         answ = input("type here --> ")
     match answ:
         case 'yes':
-            hp += 1
-            print('what type of furry are you?',
-                  'you can choose betweem \'canine\', \'feline\' abd \'other\'')
-            fur = input("type here --> ")
-            while fur != 'canine' and fur != 'feline' and fur != 'other':
-                print("you entered an invalid answer,",
-                    "plz try again")
-                fur = input("type here --> ")
-            match fur:
-                case 'canine':
-                    hp += 2
-                case 'feline':
-                    hp += 1
-                case 'other':
-                    hp -= 1
+            return True
         case 'no':
-            hp -= 1
-    print('his actual hp level is:', hp)
-    return hp
+            return False
+
+def whichFurry():
+    print('what type of furry are you?',
+            'you can choose betweem \'canine\', \'feline\' abd \'other\'')
+    fur = input("type here --> ")
+    while fur != 'canine' and fur != 'feline' and fur != 'other':
+        print("you entered an invalid answer,",
+            "plz try again")
+        fur = input("type here --> ")
+    match fur:
+        case 'canine':
+            return True
+        case 'feline':
+            return False
+        case 'other':
+            return False
